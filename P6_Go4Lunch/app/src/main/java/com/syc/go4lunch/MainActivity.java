f829236c6799e385f8f576dc614df5b1bf63cd1c
Taz91 <sylvain.chazette@orange.fr>
2020-05-21 21:21:22 +0200

    Add fct configureUI

--- P6_Go4Lunch/app/src/main/java/com/syc/go4lunch/MainActivity.java
+++ P6_Go4Lunch/app/src/main/java/com/syc/go4lunch/MainActivity.java
@@ -19,25 +19,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // ====================================== ConfigureUI
+        configureUI();
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder( R.id.nav_home, R.id.nav_gallery, R.id.nav_maps, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
         //nav_maps
+        NavigationView navigationView = findViewById(R.id.nav_view);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
@@ -45,7 +39,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
+
+    // ===================================================================================================== Configure UI
+    public void configureUI(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        mAppBarConfiguration = new AppBarConfiguration.Builder( R.id.nav_home, R.id.nav_gallery, R.id.nav_maps, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+    }
+
 }
--- P6_Go4Lunch/app/src/main/java/com/syc/go4lunch/ui/map/MapsFragment.java
+++ P6_Go4Lunch/app/src/main/java/com/syc/go4lunch/ui/map/MapsFragment.java
@@ -1,4 +1,7 @@
 package com.syc.go4lunch.ui.map;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.syc.go4lunch.R;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +13,17 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.syc.go4lunch.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MapsFragment extends Fragment {
+import static com.syc.go4lunch.R.layout.fragment_maps;
 
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+/*  onCreateView d'origine,
     private MapsViewModel mapsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
@@ -29,4 +39,53 @@ public class MapsFragment extends Fragment {
         });
         return root;
     }
+*/
+
+    private GoogleMap googleMap;
+    private MapsViewModel mapsViewModel;
+    private MapView mapView;
+
+    public MapsFragment(){}
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mapsViewModel = ViewModelProviders.of(this).get(MapsViewModel.class);
+        View root = inflater.inflate(fragment_maps, container, false);
+        final TextView textMap = root.findViewById(R.id.text_map);
+        mapsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+
+                textMap.setText(s);
+            }
+        });
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(fragment_maps);  //(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        //SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView);
+        //mapFragment.getMapAsync(this);
+
+        return root;
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
 }
